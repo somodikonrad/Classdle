4c5531c5266313ad83a74b28d013c191016c2d02
vargajanos <janosvarga706@gmail.com>
2024-02-28 11:32:41 +0100

    Klasszikus táblázat feltöltése

--- KlasszikusAloldal/js/klasszikus.js
+++ KlasszikusAloldal/js/klasszikus.js
@@ -1,5 +1,6 @@
 var app = angular.module('classdle', []);
 let select = document.querySelector('#nevek');
+let tablazat = document.querySelector('#emberek');
 
 app.run(function($rootScope){
 
@@ -16,15 +17,45 @@ app.run(function($rootScope){
             option.innerText = user.Név;
             select.appendChild(option);
     
-
-
         });
+
+        function kitoltes(){
+            
+            $rootScope.klasszikusitems.forEach(elem =>{
+                let tr = document.createElement('tr');
+        
+                let nem = document.createElement('td');
+                nem.innerText = elem.Nem;
+        
+                let magassag = document.createElement('td');
+                magassag.innerText = elem.Magasság;
+        
+                let suly = document.createElement('td');
+                suly.innerText = elem.Súly;
+        
+                let hajszin = document.createElement('td');
+                hajszin.innerText = elem.Hajszín;
+        
+                let lakhely = document.createElement('td');
+                lakhely.innerText = elem.Lakhely;
+        
+                let szuletes = document.createElement('td');
+                szuletes.innerText = elem.SzületésiÉv;
+        
+                tr.appendChild(nem);
+                tr.appendChild(magassag);
+                tr.appendChild(suly);
+                tr.appendChild(hajszin);
+                tr.appendChild(lakhely);
+                tr.append(szuletes);
+        
+                tablazat.appendChild(tr);
+            })
+            
+        }
+
 
         $rootScope.$apply();
     }); 
-
-    
-
-    
 
 });
