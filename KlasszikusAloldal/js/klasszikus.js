f819544e07d4b7051cb25af634e51ae9b1117705
vargajanos <janosvarga706@gmail.com>
2024-03-05 08:44:23 +0100

    Random ember kiválasztása

--- KlasszikusAloldal/js/klasszikus.js
+++ KlasszikusAloldal/js/klasszikus.js
@@ -1,31 +1,44 @@
 var app = angular.module('classdle', []);
 let select = document.querySelector('#nevek');
 let tablazat = document.querySelector('#emberek');
+let klasszikusitems = [];
+let random;
 
-app.run(function($rootScope){
-
-    $rootScope.klasszikusitems = [];
+app.run(function(){
 
 
     axios.get('http://localhost:3000/klasszikus').then(res => {
-        $rootScope.klasszikusitems = res.data;
-        $rootScope.klasszikusitems.sort();
+        klasszikusitems = res.data;
+        klasszikusitems.sort();
+        random = klasszikusitems[Math.floor((Math.random()*klasszikusitems.length))]
+        alert(random.Név);
 
-        $rootScope.klasszikusitems.forEach(user => {
+        klasszikusitems.forEach(user => {
             let option = document.createElement('option');
             option.value = user.Név;
             option.innerText = user.Név;
             select.appendChild(option);
     
         });
-
-        function kitoltes(){
-            
-            $rootScope.klasszikusitems.forEach(elem =>{
-                let tr = document.createElement('tr');
         
+    }); 
+
+
+});
+function kitoltes(){
+            
+        let selectesselect = document.querySelector('#nevek');
+        let nev = selectesselect.value;
+        
+
+        klasszikusitems.forEach(elem =>{
+            if(nev == elem.Név && random.Név == elem.Név)
+            {
+                let tr = document.createElement('tr');
+
                 let nem = document.createElement('td');
                 nem.innerText = elem.Nem;
+                
         
                 let magassag = document.createElement('td');
                 magassag.innerText = elem.Magasság;
@@ -50,12 +63,9 @@ app.run(function($rootScope){
                 tr.append(szuletes);
         
                 tablazat.appendChild(tr);
-            })
-            
-        }
+            }  
 
-
-        $rootScope.$apply();
-    }); 
-
-});
+       
+    })
+    
+}
