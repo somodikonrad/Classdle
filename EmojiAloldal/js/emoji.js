ac001c1ddd4bad672d70d2b63567d3504de13375
somodikonrad <somodikonrad@turr.hu>
2024-03-19 08:43:47 +0100

    Emoji aloldal folytatása

--- EmojiAloldal/js/emoji.js
+++ EmojiAloldal/js/emoji.js
@@ -1,34 +1,31 @@
 var app = angular.module('classdle', []);
 let select = document.querySelector('#nevek');
-
+let nyil = document.querySelector('#nyil');
+let emoji = document.querySelector('#emoji');
+let emojineve = "";
 let emojiItems = [];
 let random;
 
-app.run(function(){
 
+    app.run(function(){
+        axios.get('http://localhost:3000/kep').then(res => {
+            emojiItems = res.data;
+            emojiItems.sort();
+            random = emojiItems[Math.floor((Math.random()*emojiItems.length))]
+            alert(random.emojineve);
+            //alert(random.Nev);
+            emojiItems.forEach(user => {
+                let option = document.createElement('option');
+                option.value = user.Nev;
+                option.innerText = user.Nev;
+                emojineve = user.emojineve;
+                select.appendChild(option);
+            });  
+
+        }); 
+  
+    });
     
-
-
-    axios.get('http://localhost:3000/emoji').then(res => {
-        emojiItems = res.data;
-        emojiItems.sort();
-        random = emojiItems[(Math.floor(Math.random()*emojiItems.length))]
-
-        emojiItems.forEach(user => {
-            let option = document.createElement('option');
-            option.value = user.Név;
-            option.innerText = user.Név;
-            select.appendChild(option);
-    
-
-
-        });
-
-        $rootScope.$apply();
-    }); 
-
-    
-
-    
-
-});
+    function valaszt () {
+        let nev = select.value;
+    }
