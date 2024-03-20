8d777fd5ae48153fdb72900ed30fe4bbbc9ac0ed
vargajanos <janosvarga706@gmail.com>
2024-03-20 11:30:48 +0100

    Konrádnak segítés az emojiban és funkciókban ready

--- EmojiAloldal/js/emoji.js
+++ EmojiAloldal/js/emoji.js
@@ -2,23 +2,24 @@ var app = angular.module('classdle', []);
 let select = document.querySelector('#nevek');
 let nyil = document.querySelector('#nyil');
 let emoji = document.querySelector('#emoji');
-let emojineve = "";
+
+let counter = 0;
 let emojiItems = [];
 let random;
 
 
     app.run(function(){
-        axios.get('http://localhost:3000/kep').then(res => {
+        axios.get('http://localhost:3000/emoji').then(res => {
             emojiItems = res.data;
             emojiItems.sort();
             random = emojiItems[Math.floor((Math.random()*emojiItems.length))]
-            alert(random.emojineve);
-            //alert(random.Nev);
+            alert(random.Név);
+            
             emojiItems.forEach(user => {
                 let option = document.createElement('option');
-                option.value = user.Nev;
-                option.innerText = user.Nev;
-                emojineve = user.emojineve;
+                option.value = user.Név;
+                option.innerText = user.Név;
+                
                 select.appendChild(option);
             });  
 
@@ -28,4 +29,47 @@ let random;
     
     function valaszt () {
         let nev = select.value;
+        let elsoemoji = document.querySelector('#elso');
+        let masodikemoji = document.querySelector('#masodik');
+        let harmadikemoji = document.querySelector('#harmadik');
+
+        if(counter == 0)
+        {
+            elsoemoji.innerHTML = random.Emoji1 + ";";
+            counter++;
+        }  
+        else if (counter == 1) 
+        {
+            masodikemoji.innerHTML = random.Emoji2 + ";";
+            counter++;
+        }
+        else
+        {
+            harmadikemoji.innerHTML = random.Emoji3 + ";";
+        }  
+
+        if (nev === random.Név) {
+       
+            let nyil = document.querySelector('.nyil');
+            let div = document.querySelector('.frissit');
+            let refresh = document.createElement('button');
+            refresh.innerText = "Frissít";
+            refresh.onclick = function (){
+                window.location.reload();
+            }
+    
+    
+            Swal.fire({
+                title: "Nyertél!",
+                text: "Sikeresen kitaláltad kisbarátod!",
+                icon: "success"
+              });
+    
+              nyil.style.visibility = 'hidden';
+              select.style.visibility = 'hidden';
+    
+              div.appendChild(refresh);
+    
+        }
+
     }
